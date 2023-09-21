50c5f1bf55dce864663085c17037271b327b9bd4
iammyeongho <jmh4912@naver.com>
2023-09-21 17:51:46 +0900

    0921 웹 클론 코딩

---
+++ side_project/mini_board/src/insert.php
@@ -0,0 +1,71 @@
+<?
+	define("ROOT",$_SERVER["DOCUMENT_ROOT"]."/mini_board/src/");
+	define("FILE_HEADER", ROOT."header.php");
+	require_once(ROOT."lib/lib_db.php");
+
+	// POST로 request가 왔을 때 처리
+	$http_method = $_SERVER["REQUEST_METHOD"];
+	// var_dump($http_method);
+	if($http_method === "POST") {
+
+		try {
+			$arr_post = $_POST;
+			$conn = null;
+
+			if(!my_db_conn($conn))
+			{
+				throw new Exception("DB Error : PDO Instance");
+			}
+			$conn->beginTransaction();
+
+			// insert
+			if(!my_db_insert_boards($conn, $arr_post)) {
+				throw new Exception("DB Error : Insert Boards");
+			}
+			$conn->commit(); // 모든 처리 완료 시 커밋
+		}
+		catch(Exception $e) {
+			$conn->rollBack();
+			echo $e->getMessage(); // Exception 메세지 출력
+			exit;
+		}
+		
+		finally {
+			my_db_destroy_conn($conn); // DB 파기
+		}
+
+		// insert
+
+	}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/mini_board/src/css/common.css">
+	<title>작성 페이지</title>
+</head>
+<body>
+	<?php
+		require_once(FILE_HEADER);
+	?>
+	<form action="/mini_board/src/insert.php" method="post">
+		<!-- <fieldset> -->
+			<table>
+				<label for="title">제목 :</label>
+				<input type="text" name="title" id="title">
+				<br>
+				<label for="content">내용 :</label>
+				<textarea name="content" id="content" cols="30" rows="10"></textarea>
+				<br>
+				<button type="submit">작성</button>
+				<a href="/mini_board/src/list.php/">취소</a>
+			</table>
+		<!-- </fieldset> -->
+	</form>
+</body>
+</html>
--- side_project/mini_board/src/lib/lib_db.php
+++ side_project/mini_board/src/lib/lib_db.php
@@ -59,14 +59,13 @@
 	// ----------------------------
 	// 함수명 	: db_select_boards_paging
 	// 기능 	: boards paging 조회
-	// 파라미터 : PDO &$conn
+	// 파라미터 : PDO 		&$conn
+	// 			: Array 	&$arr_param | 쿼리 작성용 배열
 	// 리턴 	: Array / false
 	// ----------------------------
 
-	function my_db_select_boards_paging(&$conn) 
-	{
-		try
-		{
+	function my_db_select_boards_paging(&$conn, &$arr_param) {
+		try {
 			$sql = 
 				" SELECT "
 				."		id "
@@ -76,18 +75,90 @@
 				."		boards "
 				." ORDER BY "
 				." 		id DESC "
+				." LIMIT :list_cnt OFFSET :offset "
 				;
-			$arr_ps = [];
+			$arr_ps = 
+			[
+				":list_cnt" => $arr_param["list_cnt"]
+				,":offset" => $arr_param["offset"]
+			];
 
 			$stmt = $conn->prepare($sql);
 			$stmt->execute($arr_ps);
 			$result = $stmt->fetchAll();
 			return $result; // 정상 : 쿼리 결과 리턴
 		}
-		catch(Exception $e)
-		{
+		catch(Exception $e) {
 			return false; // 예외 발생 : flase 리턴
 		}
 	}
+
+	// ----------------------------
+	// 함수명 	: db_select_boards_paging
+	// 기능 	: boards count 조회
+	// 파라미터 : PDO 		&$conn
+	// 리턴 	: int / false
+	// ----------------------------
+
+	function my_db_select_boards_cnt(&$conn) {
+		$sql =
+			" SELECT "
+			." 		count(id) as cnt "
+			." FROM "
+			." 		boards "
+			;
+			
+		try {
+			$stmt = $conn->query($sql);
+			$result = $stmt->fetchAll();
+			
+			return (int)$result[0]["cnt"];
+		}
+		catch(Exception $e) {
+			return false; // 예외 발생 : flase 리턴
+		}
+	}
+
+
+		// ----------------------------
+	// 함수명 	: my_db_insert_boards
+	// 기능 	: boards 레코드 작성
+	// 파라미터 : PDO 		&$conn
+	// 			: Array 	&$arr_param | 쿼리 작성용 배열
+	// 리턴 	: Boolean
+	// ----------------------------
+
+	function my_db_insert_boards(&$conn, &$arr_param) {
+		$sql = 
+			" INSERT INTO boards ( "
+			."		title "
+			."		,content "
+			." ) "
+			." VALUES ( "
+			."		:title "
+			."		,:content "
+			." ) "
+			;
+
+		$arr_ps = [
+			":title" => $arr_param["title"]
+			,":content" => $arr_param["content"]
+		];
+
+		try {
+			$stmt = $conn->prepare($sql);
+			$result = $stmt->execute($arr_ps);
+			return $result; // 결과 리턴
+		}
+		catch(Exception $e) {
+			return false; // 예외 발생 : flase 리턴
+		}
+	}
+
+
+	// $conn = null;
+	// my_db_conn($conn);
+	// echo my_db_select_boards_cnt($conn);
+	// $conn = null;
 
 ?>

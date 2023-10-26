b4cd845bf4d8a42ba06cc4c1e5779fa8be913eba
iammyeongho <jmh4912@naver.com>
2023-10-26 23:12:04 +0900

    1026 자바스크립트

--- javascript/Tng/40.js
+++ javascript/Tng/40.js
@@ -10,49 +10,73 @@
 // 4. 3번의 상태에서 다시 한번 더 클릭하면 아래의 알러트를 출력하고, 배경색이 흰색으로 안보이게 됩니다
 // 다시 숨는다
 
-const SUBURB = document.querySelector('#suburb');
+// const SUBURB = document.querySelector('#suburb');
+// const INTERIOR = document.querySelector('#interior');
+// let i = 0;
 
-SUBURB.addEventListener('mouseenter', divmous);
-function divmous() {
-	alert('두근두근');
-}
-
-// SUBURB.addEventListener('mouseenter', () => {
-// 		alert('두근두근');
-// });
-
-
-const INTERIOR = document.querySelector('#interior');
-// INTERIOR.onclick = function() {
-// 	alert('들켰다');
-// 	INTERIOR.style.backgroundColor = '#000000';
-
-// 	// var x = Math.floor(Math.random() * window.innerWidth);
-//     // var y = Math.floor(Math.random() * window.innerHeight);
-
-//     // // div의 위치 변경
-//     // INTERIOR.style.left = x + 'px';
-//     // INTERIOR.style.top = y + 'px';
+// SUBURB.addEventListener('mouseenter', divmous);
+// function divmous() {
+//     if (i === 0) {
+//         alert('두근두근');
+//     }
 // }
 
+// INTERIOR.addEventListener('click', divClick);
+// function divClick() {
+//     if (i === 0) {
+//         alert('들켰다');
+//         INTERIOR.style.backgroundColor = '#000000';
+//         i = 1; // 들켰다 상태로 변경
+//         INTERIOR.removeEventListener('click', divClick);
+//         SUBURB.removeEventListener('mouseenter', divmous);
+//         INTERIOR.addEventListener('click', divClick2);
+//     }
+// }
+
+// function divClick2() {
+//     alert('다시 숨는다');
+//     INTERIOR.style.backgroundColor = '#ffffff';
+
+//     var x = Math.floor(Math.random() * window.innerWidth);
+//     var y = Math.floor(Math.random() * window.innerHeight);
+
+//     // div의 위치 변경
+//     SUBURB.style.left = x + 'px';
+//     SUBURB.style.top = y + 'px';
+    
+//     i = 0; // 다시 숨는다 상태로 변경
+// }
+
+const SUBURB = document.querySelector('#suburb');
+const INTERIOR = document.querySelector('#interior');
+let i = true;
+// let i_false = true;
+
+SUBURB.addEventListener('mouseenter', divmous);
+
+function divmous() {
+    if (i) {
+        alert('두근두근');
+    }
+}
+
 INTERIOR.addEventListener('click', divClick);
+
 function divClick() {
-	alert('들켰다');
-	INTERIOR.style.backgroundColor = '#000000';
-	INTERIOR.removeEventListener('click', divClick);
-	SUBURB.removeEventListener('mouseenter', divmous);
-	INTERIOR.addEventListener('click', divClick2);
+    if (i) {
+        alert('들켰다');
+        INTERIOR.style.backgroundColor = '#000000';
+    } else {
+        alert('다시 숨는다');
+        INTERIOR.style.backgroundColor = '#ffffff';
+
+        var x = Math.floor(Math.random() * window.innerWidth);
+        var y = Math.floor(Math.random() * window.innerHeight);
+
+        // INTERIOR 위치 변경
+        SUBURB.style.left = x + 'px';
+        SUBURB.style.top = y + 'px';
+    }
+
+    i = !i;
 }
-function divClick2() {
-	alert('다시 숨는다');
-	INTERIOR.style.backgroundColor = '#ffffff';
-
-	var x = Math.floor(Math.random() * window.innerWidth);
-    var y = Math.floor(Math.random() * window.innerHeight);
-
-    // div의 위치 변경
-    SUBURB.style.left = x + 'px';
-    SUBURB.style.top = y + 'px';
-}
-
-

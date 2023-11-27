6b1df4f28dede48aaa270fd34ce5ed0ffceb35d2
iammyeongho <jmh4912@naver.com>
2023-11-27 19:30:41 +0900

    1127 포트폴리오 작업 로그인 화면 단일

--- laravel_vue/routes/web.php
+++ laravel_vue/routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 넘겨주는 데이터는 제이슨으로 인코딩해서 리턴해야함 json_encode($data)
+
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'name' => 'root',
+        'id' => 1,
+    ];
+    return view('welcome')->with('data', json_encode($data));
+});
+
+
+Route::get('/login', function () {
+    $data = [
+        'name' => 'login',
+        'id' => 1,
+    ];
+    return view('welcome')->with('data', json_encode($data));
+});
+
+
+Route::get('/board', function () {
+    $data = [
+        'name' => 'board',
+        'id' => 1,
+    ];
+    return view('welcome')->with('data', json_encode($data));
 });
